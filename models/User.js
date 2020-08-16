9471f4db54e70345236c794ae56ff4c1ddb8763b
danh phi long <long9ka@gmail.com>
2020-08-16 15:34:54 +0700

    user schema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserShema = new mongoose.Shema({
+  username: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    enum: ["quan_ly", "le_tan", "nha_hang", "guest"]
+    default: "guest"
+  }}
+});
+
+module.exports = mongoose.modle("user", UserSchema);
