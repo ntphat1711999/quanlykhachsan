1f483455c788a485fd6f54567a4f9764a6f9d3cf
danh phi long <long9ka@gmail.com>
2020-08-16 15:26:32 +0700

    them middleware

--- routes/room.route.js
+++ routes/room.route.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 
+// mws
+const auth = require("../middlewares/auth");
+
 // models
 const Room = require("../models/room.model");
 
 router.route("/")
-  .get((req, res) => {
+  .get(auth, (req, res) => {
     res.send("room");
   });
 
